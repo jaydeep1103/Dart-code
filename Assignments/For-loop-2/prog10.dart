a2bdb481bb993ae9054fd880b3b4136f53045342
Jaydeep More <jaydeep110315@gmail.com>
2024-08-17 17:11:54 +0530

    forloop-2

---
+++ Assignments/For-loop-2/prog10.dart
@@ -0,0 +1,12 @@
+void main(){
+  
+  for(int i=20;i<=70;i++){
+    if(i%2!=0){
+      print(i*i);
+    }
+    else if(i%2==0){
+      print(i*i*i);
+    }
+  }
+  
+}
---
+++ Assignments/For-loop-2/prog3.dart
@@ -0,0 +1,8 @@
+void main(){
+  for(int i=20; i<=120;i++){
+    if(i%2!=0){
+      print(i);
+    }
+    
+  }
+}
