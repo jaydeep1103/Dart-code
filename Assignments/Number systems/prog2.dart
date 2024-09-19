6b91791181149322cba9eeb0457a7477aec983e9
Jaydeep More <jaydeep110315@gmail.com>
2024-09-19 13:05:08 +0530

    number systems

---
+++ Assignments/Number systems/prog2.dart
@@ -0,0 +1,15 @@
+import 'dart:io';
+void main(){
+  stdout.write("Enter the Number : ");
+  int num = int.parse(stdin.readLineSync()!);
+  bool flag = true;
+  for(int i =2;i<num~/2;i++){
+    if(num % i ==0){
+      stdout.writeln("$num is Not a Prime Number.");
+      flag =false;
+    }
+  }
+  if(flag == true){
+  print("$num is Prime Number.");
+}
+}
---
+++ Assignments/Number systems/prog4.dart
@@ -0,0 +1,31 @@
+import 'dart:io';
+void main(){
+  stdout.write("Enter the number : ");
+  int number =int.parse(stdin.readLineSync()!);
+  int sum = 0;
+  int temp = number;
+  int numDigits = 0;
+
+  while (temp > 0) {
+    numDigits++;
+    temp ~/= 10;
+  }
+  temp=number;
+  while (temp > 0) {
+    int digit = temp % 10;
+    int power = 1;
+    for (int i = 0; i < numDigits; i++) {
+      power *= digit;
+    }
+
+    sum += power;
+    temp ~/= 10;
+  }
+
+  if(number == sum){
+    print("$number is Armstrong number");
+  }else{
+    print("$number is not Armstrong number");
+  }
+
+}
